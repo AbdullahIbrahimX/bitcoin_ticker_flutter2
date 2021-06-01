a9c629bf05e98b91647cc307897271dc1b32d137
SafLabs <alsafwan.abdullah@gmail.com>
2021-06-01 16:02:37 +0300

    1/6/2021

--- lib/services/kracken_ws.dart
+++ lib/services/kracken_ws.dart
@@ -14,12 +14,13 @@ const Map<String, Map> KrakenEvents = {
   }
 };
 
-class KrackenWS {
+class KrakenWS {
   static IOWebSocketChannel _channel;
   static Stream _stream;
   static String _uri = 'wss://ws.kraken.com';
   static List<String> _cryptoPairs = [];
   static var _codec = new JsonCodec().fuse(new Utf8Codec());
+  static Map _coinDisplayData = {};
 
   static subscribeToPairs(String currency, List<String> cryptoList) {
     _init();
@@ -63,11 +64,35 @@ class KrackenWS {
   }
 
   static Stream listenToCoinPrices2() async* {
-    Stream coinData =
+    Stream stream =
         _stream.where((event) => jsonDecode(event) is List<dynamic>);
 
-    await for (var coinPayload in coinData) {
-      print('FROM LISTEN TO COIN $coinPayload');
+    await for (String coinPrePayload in stream) {
+      List coinPayload = jsonDecode(coinPrePayload);
+      Map coinData = coinPayload[1];
+      double closePrice = double.parse(coinData['c'][0]);
+      String closeVolume = coinData['c'][1];
+      double openPrice = double.parse(coinData['o'][0]);
+      String pairName = coinPayload.last.toString();
+      double prevClosePrice = _coinDisplayData[pairName] == null
+          ? 0.0
+          : _coinDisplayData[pairName][0];
+
+      double changeAmount = closePrice - prevClosePrice;
+      double OAchangePercentage =
+          (100 * ((closePrice - openPrice) / openPrice));
+
+      _coinDisplayData[pairName] = [
+        closePrice,
+        closeVolume,
+        openPrice,
+        pairName,
+        changeAmount,
+        OAchangePercentage
+      ];
+      print(_coinDisplayData);
+
+      yield _coinDisplayData;
     }
   }
 
@@ -84,83 +109,17 @@ class KrackenWS {
     _stream = _channel.stream.asBroadcastStream();
   }
 }
-//
-// class KrackenWS {
-//   static String _uri = 'wss://ws.kraken.com';
-//   static List<String> _crypto;
-//   static String _currency;
-//   static List<String> _pairs = [];
-//   static IOWebSocketChannel _channel;
-//   static var _codec = new JsonCodec().fuse(new Utf8Codec());
-//
-//   static _initialize() async {
-//     if (_channel != null) return;
-//     if (_crypto == null || _currency == null) {
-//       print('no currency or crypto');
-//       return;
-//     }
-//     _channel = IOWebSocketChannel.connect(Uri.parse(_uri),
-//         pingInterval: Duration(seconds: 4));
-//     print('initialized');
-//   }
-//
-//   static Stream<List> listen() async* {
-//     _initialize();
-//     _channel.stream.listen((event) {
-//       dynamic message = jsonDecode(event);
-//       print(message);
-//
-//       if (message.isNotEmpty) {
-//         if (message is List || message is List<dynamic>) {
-//           print('list');
-//           print(message);
-//         } else {
-//           print('Message type : ${message.runtimeType}');
-//           if (message.containsKey('event')) {
-//             print(message['event']);
-//             if (message['event'] == 'systemStatus' &&
-//                 message['status'] == 'online') {
-//               print('sub');
-//               List<int> subEvent =
-//                   _codec.encode({...KrakenEvents['subscribe'], 'pair': _pairs});
-//               _channel.sink.add(subEvent);
-//             }
-//           }
-//         }
-//       }
-//     });
-//   }
-//
-//   static subscribe(List<String> crypto, String currency) {
-//     _crypto = crypto;
-//     _currency = currency;
-//
-//     _crypto.forEach((element) {
-//       _pairs.add('$element/$_currency');
-//     });
-//   }
-//
-//   static unsubscribe() {
-//     _initialize();
-//     print('subscribed');
-//     List<int> event = _codec.encode({
-//       ...KrakenEvents['unsubscribe'],
-//       'pair': ["BTC/USD"]
-//     });
-//     _channel.sink.add(event);
-//   }
-//
-//   static stopListening() async {
-//     _channel.sink.close(status.goingAway);
-//     await _channel.sink.done;
-//     _channel = null;
-//   }
-//
-//   static List<String> _generatePairs() {
-//     List<String> gen = [];
-//
-//     return gen;
-//   }
-//
-//   static _handleSocketMessages() {}
-// }
+
+// _coinPrices.update(
+// cryptoName,
+// (value) => [
+// closePrice,
+// closeVolume,
+// openPrice,
+// pairName,
+// changeAmount.toStringAsFixed(5),
+// OAchangePercentage.toStringAsFixed(2),
+// compare(closePrice, _coinPrices[cryptoName][0]),
+// compare(OAchangePercentage.toString(),
+// _coinPrices[cryptoName][0])
+// ]);
